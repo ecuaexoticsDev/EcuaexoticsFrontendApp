4c26b4de56d272145d30c5450425b949821eec52
juanrjc97 <juanj121297@gmail.com>
2022-02-18 18:22:16 -0500

    commit cambio en la url enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,14 @@
 
 export const environment = {
   production: false,
-  base_url:  'http://127.0.0.1:8000/api/',
-  // base_url: 'https://ecuaexotics.pythonanywhere.com/api/',
-  backend_url: 'http://127.0.0.1:8000/',
-  // backend_url: 'https://ecuaexotics.pythonanywhere.com/',
+  base_url: 'https://ecuaexotics.pythonanywhere.com/api/',
+  backend_url: 'https://ecuaexotics.pythonanywhere.com/',
 };
 
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
+ *'http://127.0.0.1:8000/api/',
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
