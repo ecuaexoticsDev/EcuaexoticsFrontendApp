02f63a5302e7a159f6128ad4e66767cf7d40772c
juanrjc97 <juanj121297@gmail.com>
2022-03-16 15:48:27 -0500

    merge realizado y actualizado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://127.0.0.1:8000/api/',//'https://ecuaexotics.pythonanywhere.com/api/',
-  backend_url: 'http://127.0.0.1:8000/'//'https://ecuaexotics.pythonanywhere.com/',
+  base_url: 'https://ecuaexotics.pythonanywhere.com/api/',
+  backend_url: 'https://ecuaexotics.pythonanywhere.com/',
 };
 
 /*
